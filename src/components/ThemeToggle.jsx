02bca4083600ccca3fd9c98acd38d6aef2c73dd0
shoalam <shoalamdu@gmail.com>
2025-02-20 02:07:12 +0600

    Theme Toggle button border fixed

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -10,7 +10,7 @@ const ThemeToggle = () => {
 
   return (
     <button
-      className="p-2 rounded-lg w-8 h-8 border border-1 border-gray-500 dark:border-gray-100 flex justify-center items-center absolute left-3 top-3"
+      className="p-2 rounded-lg w-8 h-8 border border-1 border-gray-500 dark:border-gray-700 flex justify-center items-center absolute left-3 top-3"
       onClick={handleThemeChange}
     >
       {theme === "dark" ? <span>🌞</span> : <span>🌙</span>}
